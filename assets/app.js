c6cd62f170001d8724347b23cfe5c4d6be613250
David LEROUX <dleroux63160@gmail.com>
2022-11-25 13:43:43 +0100

    home page css

--- assets/app.js
+++ assets/app.js
@@ -4,7 +4,25 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
+"use strict";
 
+
+let sidenav = document.getElementById("mySidenav");
+let openBtn = document.getElementById("openBtn");
+let closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    sidenav.classList.add("active");
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    sidenav.classList.remove("active");
+}
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
